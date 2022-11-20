fdc9937586746bd8d499e30465ebb141bcc99016
gtgambit <gtgambit@gmail.com>
2022-11-20 22:37:51 +0200

    update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,7 @@
-import Notiflix from "notiflix";
-
-export const fetchCountries = (countryName) => {
+export const fetchCountries = countryName => {
   return fetch(
     `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
-  ).then((response) => {
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
